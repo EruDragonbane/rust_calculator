879f2b77d836e40511427adebe226c014f64b35c
Eru Dragonbane <akkurt_fatih@hotmail.com>
2022-10-19 16:14:08 +0300

    test for int-> float and float->int converts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-type CalcInt = i64;
+type CalcInt = f64;
 /// Parse, Token Index and Errors
 struct ParseState {
     line: String,
@@ -24,28 +24,28 @@ fn lex_match (ps: &mut ParseState, expected: char) {
 /// Parse, Token Index and Errors
 /// Digit Parse
 fn scan_digits (ps: &mut ParseState) -> CalcInt {
-    const BASE: CalcInt = 10;
-    let mut value: CalcInt = 0;
+    const BASE: CalcInt = 10.0;
+    let mut value: CalcInt = 0.0;
     loop {
         let digit: CalcInt;
         match token(ps) {
-            '0' => digit = 0,
-            '1' => digit = 1,
-            '2' => digit = 2,
-            '3' => digit = 3,
-            '4' => digit = 4,
-            '5' => digit = 5,
-            '6' => digit = 6,
-            '7' => digit = 7,
-            '8' => digit = 8,
-            '9' => digit = 9,
+            '0' => digit = 0.0,
+            '1' => digit = 1.0,
+            '2' => digit = 2.0,
+            '3' => digit = 3.0,
+            '4' => digit = 4.0,
+            '5' => digit = 5.0,
+            '6' => digit = 6.0,
+            '7' => digit = 7.0,
+            '8' => digit = 8.0,
+            '9' => digit = 9.0,
             _ => break
         }
         if digit >= BASE {
             bad_formula(format!("Digit {digit} out of range for base {BASE}"));
         }
-        if value > (std::i64::MAX - digit)/BASE {
-            bad_formula(format!("Integer overflow"));
+        if value > (std::f64::MAX - digit)/BASE {
+            bad_formula(format!("Float overflow"));
         }
         value = value*BASE+digit;
         ps.index += 1;
@@ -53,7 +53,7 @@ fn scan_digits (ps: &mut ParseState) -> CalcInt {
     return value
 }
 /// Digit Parse
-///  Operators
+/// Operators
 fn brackets (ps: &mut ParseState) -> CalcInt {
     let value: CalcInt;
     if token(ps) == '(' {
@@ -65,7 +65,7 @@ fn brackets (ps: &mut ParseState) -> CalcInt {
         value = scan_digits(ps);
     }
     else {
-        value = 0;
+        value = 0.0;
         bad_formula("Bad Formula".to_string());
     }
     return value
@@ -73,12 +73,12 @@ fn brackets (ps: &mut ParseState) -> CalcInt {
 fn exponent (ps: &mut ParseState) -> CalcInt {
     let mut value: CalcInt = brackets(ps);
     let exponent_value: CalcInt = value;
-    let mut counter: CalcInt = 1;
+    let mut counter: i64 = 1;
     if token(ps) == '^' {
         lex_match(ps, '^');
-        let pow: CalcInt = brackets(ps);
+        let pow: i64 = brackets(ps).round() as i64;
         if pow == 0 {
-            return 1
+            return 1 as f64
         }
         else if pow == 1 {
             return value
@@ -106,12 +106,12 @@ fn multiply_divide_modulo (ps: &mut ParseState) -> CalcInt {
             },
             '%' => {
                 lex_match(ps, '%');
-                let modulo: CalcInt = exponent(ps);
+                let modulo: i64 = exponent(ps).round() as i64;
                 if modulo == 0 {
-                    return 0
+                    return 0 as f64
                 } 
                 else {
-                    value %= modulo
+                    value %= modulo as f64
                 }
             },
             _ => {},
